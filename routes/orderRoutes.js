90eebefec5a90a8c914890cdb5d84c2b76ccc1fc
Nur A Alam <nuraalam376@gmail.com>
2020-10-15 08:54:16 +0600

    Get all orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,6 +3,7 @@ module.exports = (app, orderCollection) => {
   app.post("/addorder", (req, res) => {
     const { name, email, price, detail, service } = req.body;
     const projectFile = req.files.project;
+    const status = "pending";
 
     const projectImageFile = projectFile.data;
     const encodedProjectImage = projectImageFile.toString("base64");
@@ -14,10 +15,22 @@ module.exports = (app, orderCollection) => {
     };
 
     orderCollection
-      .insertOne({ name, email, service, detail, price, projectImage })
+      .insertOne({ name, email, service, detail, price, projectImage, status })
       .then((result) => {
         return res.send(result.insertedCount > 0);
       })
       .catch((err) => console.log(err));
+  });
+
+  // Gets all orders of the current user
+  app.post("/allorders", (req, res) => {
+    orderCollection
+      .find({ email: req.body.email })
+      .toArray((err, documents) => {
+        if (!err) {
+          return res.send(documents);
+        }
+        res.send(null);
+      });
   });
 };
